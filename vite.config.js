c5d4504119c577dcb3fe2210234599411233271e
harsha1826 <2300031826@kluniversity.in>
2025-10-08 21:35:54 +0530

    Add GitHub Actions deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/cicd-project-frontend/',
   plugins: [react()],
-  base: '/car-frontend/',
+ 
 })
